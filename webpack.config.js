4fce69ec2af706459a686d1f0c81c2dcfa222602
Piotr Mamenas <piotr.mamenas@gmail.com>
2018-02-23 23:50:56 +0100

    Finally getting bootstrap to work with webpack bundling, everything gets distributed into dist properly, but theres still a hmr error popping up in the middleware which needs to be patched.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const bootstrapEntryPoints = require('./webpack.bootstrap.config.js');
+
+// eslint-disable-next-line no-console
+console.log(`=> bootstrap-loader configuration: ${bootstrapEntryPoints.dev}`);
+
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: [
+    'tether',
+    'font-awesome-loader',
+    bootstrapEntryPoints.dev,
+    './src/app.js'
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
@@ -11,7 +23,36 @@ module.exports = {
     new BrowserSyncPlugin({
       host: 'localhost',
       port: 3000,
-      server: { baseDir: ['public'] }
+      server: { baseDir: ['dist'] }
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.ProvidePlugin({
+      'window.Tether': 'tether',
+    }),
+    new webpack.LoaderOptionsPlugin({
+      postcss: [autoprefixer],
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
     })
-  ]
+  ],
+  devtool: '#cheap-module-eval-source-map',
+  resolve: { extensions: ['*', '.js'] },
+  watch: true,
+  module: {
+      rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'] },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+      { test: /bootstrap-sass\/assets\/javascripts\//, use: 'imports-loader?jQuery=jquery' },
+    ],
+  },
 };
