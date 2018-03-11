2cab4c066296b5fd86d6545d4e1036475d10a08d
Piotr Mamenas <piotr.mamenas@gmail.com>
2018-03-11 21:54:39 +0100

    Slight fix, the blog component was not properly reloading on route change.

--- src/PersonalPage/PersonalPage.Web/ClientApp/app/components/blog/blog.component.ts
+++ src/PersonalPage/PersonalPage.Web/ClientApp/app/components/blog/blog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
 import { Http } from '@angular/http';
 import { ActivatedRoute } from '@angular/router';
 import { PostService } from './../../services/post.service';
@@ -9,23 +9,20 @@ import { TagService } from './../../services/tag.service';
     templateUrl: './blog.component.html',
     styleUrls: ['./blog.component.css']
 })
-export class BlogComponent implements OnInit, OnDestroy {
+export class BlogComponent implements OnDestroy {
     posts: any[];
     tags: any[];
-    routeParams: string;
 
     constructor(private postService: PostService, private tagService: TagService, private route: ActivatedRoute) {
-    }
+        route.params.subscribe(params => {
+            this.tagService.getTags().subscribe(tags => this.tags = tags);
 
-    ngOnInit() {
-        this.tagService.getTags().subscribe(tags => this.tags = tags);
-        this.route.params.subscribe(params => this.routeParams = params.tagName);
-
-        if (this.routeParams == null) {
-            this.postService.getPosts().subscribe(posts => this.posts = posts);
-        } else {
-            this.postService.getPostsByTag(this.routeParams).subscribe(posts => this.posts = posts);
-        }
+            if (params.tagName == null) {
+                this.postService.getPosts().subscribe(posts => this.posts = posts);
+            } else {
+                this.postService.getPostsByTag(params.tagName).subscribe(posts => this.posts = posts);
+            }
+        });
     }
 
     ngOnDestroy() {
--- src/PersonalPage/PersonalPage.Web/Migrations/20180311203430_SmallFixMigration.Designer.cs
+++ src/PersonalPage/PersonalPage.Web/Migrations/20180311203430_SmallFixMigration.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace PersonalPage.Web.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180311203430_SmallFixMigration")]
+    partial class SmallFixMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
