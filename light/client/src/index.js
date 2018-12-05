6e074045c50a0e01df1249f65c6e6b7c25fa09db
Piotr <piotr.mamenas@gmail.com>
2018-12-05 22:00:42 +0100

    bootstrap + header

--- light/client/src/index.js
+++ light/client/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-  return <div id='app'></div>
+  return (
+    <div id='app'>
+      <nav class="navbar fixed-top bg-faded">
+        <a class="navbar-brand" href="#">Logo</a>
+      </nav>
+    </div>
+  );
 };
 
 ReactDOM.render(
